df0ffc5d29d95a059d6635c83a26500657cc81f8
Yan Gustavo Pegyn Silva <yanpegyn@hotmail.com>
2022-01-09 20:39:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 
-
-
 const app = express();
-app.use(cors())
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use('/funcionario', require('./routes/CRUD_Funcionario')(app))
-    .use('/cliente', require('./routes/Cliente')(app))
-    ;
-
 global.sequelize;
 db(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST, process.env.DB_DIALECT).then(
     (sequelize) => {
         if (sequelize) {
+            app.use(cors())
+                .use(express.json())
+                .use(express.urlencoded({ extended: true }))
+                .use('/funcionario', require('./routes/CRUD_Funcionario')(app))
+                .use('/cliente', require('./routes/Cliente')(app))
+                ;
             app.listen(PORT, () => console.log(`Listening on ${PORT}`));
             global.sequelize = sequelize;
         }
