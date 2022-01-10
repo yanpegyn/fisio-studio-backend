cd2dc7f65ab50e2f55cf538fb64be5302f14800c
Yan Gustavo Pegyn Silva <yanpegyn@hotmail.com>
2022-01-09 21:15:18 -0300

    Update Login.js

--- middlewares/Login.js
+++ middlewares/Login.js
@@ -1,9 +1,9 @@
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const { nome_de_usuario, senha } = req.body.credenciais;
     const where = { nome_de_usuario };
     const adm = await global.sequelize.models.Funcionario.findOne({ where });
     let valid = (adm && (await adm.efetuaLogin(senha))) ? true : false;
-    if(!valid) return res.status(401).send({ message: 'Usuário ou senha incorreto' }).end();
+    if (!valid) return res.status(401).send({ message: 'Usuário ou senha incorreto' }).end();
     res.locals.user = adm.user;
     next();
 };
