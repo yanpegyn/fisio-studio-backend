6a3d979092bd19e5ad4f001fc2a2587680b8e4ea
Yan Gustavo Pegyn Silva <yanpegyn@hotmail.com>
2022-01-09 23:26:14 -0300

    Update Login.js

--- middlewares/Login.js
+++ middlewares/Login.js
@@ -1,10 +1,14 @@
 module.exports = async (req, res, next) => {
-    const { nome_de_usuario, senha } = req.body.credenciais;
-    const where = { nome_de_usuario };
-    const adm = await global.sequelize.models.Funcionario.findOne({ where });
-    let valid = (adm && (await adm.efetuaLogin(senha))) ? true : false;
-    if (!valid) return res.status(401).send({ message: 'Usuário ou senha incorreto' }).end();
-    res.locals.user = adm.user;
-    next();
+    try {
+        const { nome_de_usuario, senha } = req.body.credenciais;
+        const where = { nome_de_usuario };
+        const adm = await global.sequelize.models.Funcionario.findOne({ where });
+        let valid = (adm && (await adm.efetuaLogin(senha))) ? true : false;
+        if (!valid) return res.status(401).send({ message: 'Usuário ou senha incorreto' }).end();
+        res.locals.user = adm.user;
+        next();
+    } catch (err) {
+        return res.status(403).end();
+    }
 };
 ///Definir os privilégios
