f69b11982ebe3b9d5e7830bc2725d1631994ee83
Yan Gustavo Pegyn Silva <yanpegyn@hotmail.com>
2022-03-05 16:49:47 -0300

    Update Cliente.js

--- controllers/Cliente.js
+++ controllers/Cliente.js
@@ -95,7 +95,7 @@ module.exports.aniversariantes = async (req, res) => {
     const convertDate = (date) => date.split("T")[0].split("-").filter((_v, i) => i > 0);
     const isNumeric = (input) => (input - 0) == input && ("" + input).length > 0;
     try {
-        const { page, pageSize, mes, dia } = req.query;
+        const { page, pageSize, mes, dia, dia_fim } = req.query;
         const offset = parseInt(page) * parseInt(pageSize);
         const limit = parseInt(pageSize);
         const clientes = await (async () => {
@@ -104,11 +104,14 @@ module.exports.aniversariantes = async (req, res) => {
                 if (isNumeric(mes) && isNumeric(dia) && (1 <= mes && mes <= 12) && (1 <= dia && dia <= 31)) {
                     hj = [mes, dia];
                 } else hj = convertDate(new Date().toISOString());
+                if (dia_fim && isNumeric(dia_fim) && (1 <= dia_fim && dia_fim <= 31)) {
+                    hj[2] = dia_fim;
+                } else hj[2] = hj[1];
                 return await Cliente.findAndCountAll({
                     where: {
                         [Op.and]: [
                             sequelize.where(sequelize.fn('month', sequelize.col('data_de_nascimento')), hj[0]),
-                            sequelize.where(sequelize.fn('day', sequelize.col('data_de_nascimento')), hj[1])
+                            sequelize.where(sequelize.fn('day', sequelize.col('data_de_nascimento')), { [Op.between]: [hj[1], hj[2]] })
                         ]
                     },
                     offset,
